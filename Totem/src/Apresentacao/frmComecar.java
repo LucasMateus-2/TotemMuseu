c66ea95e093edb2d1f11e7ab43eb74c902cd7bf4
Lucas Mateus <lucasmateusrossi1@gmail.com>
2024-05-26 15:48:04 -0300

    Formulário de Exposições

--- Totem/src/Apresentacao/frmComecar.java
+++ Totem/src/Apresentacao/frmComecar.java
@@ -147,7 +147,7 @@ public class frmComecar extends javax.swing.JDialog {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         this.dispose();
-        frmExpoesicoes frmExp = new frmExpoesicoes(null, true);
+        frmExposicoes frmExp = new frmExposicoes(null, true);
         frmExp.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
--- Totem/src/Apresentacao/frmExposicoes.java
+++ Totem/src/Apresentacao/frmExposicoes.java
@@ -8,12 +8,12 @@ package Apresentacao;
  *
  * @author lucasmateus
  */
-public class frmExpoesicoes extends javax.swing.JDialog {
+public class frmExposicoes extends javax.swing.JDialog {
 
     /**
      * Creates new form teste
      */
-    public frmExpoesicoes(java.awt.Frame parent, boolean modal) {
+    public frmExposicoes(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -478,21 +478,23 @@ public class frmExpoesicoes extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmExpoesicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmExposicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmExpoesicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmExposicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmExpoesicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmExposicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmExpoesicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmExposicoes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                frmExpoesicoes dialog = new frmExpoesicoes(new javax.swing.JFrame(), true);
+                frmExposicoes dialog = new frmExposicoes(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
